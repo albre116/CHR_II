f237a2fdfbeb31187713b398051e09bf78af462b
albre116 <albre116@gmail.com>
2015-02-10 21:48:33 -0600

    working map code with bootstrap3

--- trunk/ShinyDeployModel/server.R
+++ trunk/ShinyDeployModel/server.R
@@ -6,6 +6,7 @@ shinyServer(function(input, output, session) {
       OriginAdd<- reactiveValues(x=NULL, y=NULL)
       OriginDelete<- reactiveValues(x=NULL, y=NULL)
 
+
       
       observe({
         # Initially will be empty
@@ -25,21 +26,20 @@ shinyServer(function(input, output, session) {
         isolate(OriginDelete$y <- c(OriginDelete$y, input$DeleteOrigin$y))
       })
       
-      
       DATASTATES <- reactive({
         if(!is.null(input$AddOrigin)){
-        AddOrigStates <- data.frame(state=map.where(states,
-                                                    x=OriginAdd$x,
-                                                    y=OriginAdd$y))
-        AddOrigStates$Value <- 1
-        AddOrigStates <- AddOrigStates[complete.cases(AddOrigStates),]
+          AddOrigStates <- data.frame(state=map.where(states,
+                                                      x=OriginAdd$x,
+                                                      y=OriginAdd$y))
+          AddOrigStates$Value <- 1
+          AddOrigStates <- AddOrigStates[complete.cases(AddOrigStates),]
         } else{AddOrigStates <- data.frame(state="California",Value=0)}
         if(!is.null(input$DeleteOrigin)){
-        DeleteOrigStates <- data.frame(state=map.where(states,
-                                                       x=OriginDelete$x,
-                                                       y=OriginDelete$y))
-        DeleteOrigStates$Value <- -1
-        DeleteOrigStates <- DeleteOrigStates[ complete.cases(DeleteOrigStates),]
+          DeleteOrigStates <- data.frame(state=map.where(states,
+                                                         x=OriginDelete$x,
+                                                         y=OriginDelete$y))
+          DeleteOrigStates$Value <- -1
+          DeleteOrigStates <- DeleteOrigStates[ complete.cases(DeleteOrigStates),]
         } else{DeleteOrigStates <- data.frame(state="California",Value=0)}
         
         ####figure out the current state of the clicks by mappking (k,v) pairs
@@ -48,7 +48,7 @@ shinyServer(function(input, output, session) {
           summarise(count=sum(Value) )%>% 
           filter(count>=1)
         if(nrow(STATES)>0){
-        mapOrig <- map("state",regions = STATES$state,plot=F,fill=T,col="grey")} else{mapOrig <- NULL}
+          mapOrig <- map("state",regions = STATES$state,plot=F,fill=T,col="grey")} else{mapOrig <- NULL}
         list(mapOrig=mapOrig)
       })
       
@@ -60,8 +60,8 @@ shinyServer(function(input, output, session) {
         }
         map.text(state_labs,add=T)
       })
-        
-        
+      
+      
       output$OrigPlotDelete <- renderPlot(function(){
         mapOrig <- DATASTATES()[["mapOrig"]]
         map(states)
@@ -69,7 +69,78 @@ shinyServer(function(input, output, session) {
           map(mapOrig,fill=T,add=T,col="grey")
         }
         map.text(state_labs,add=T)
+      })
+      
+      
+      ####do the destination stuff
+      DestAdd<- reactiveValues(x=NULL, y=NULL)
+      DestDelete<- reactiveValues(x=NULL, y=NULL)
+      
+      observe({
+        # Initially will be empty
+        if (is.null(input$AddDest)){
+          return() 
+        }
+        isolate(DestAdd$x <- c(DestAdd$x, input$AddDest$x))
+        isolate(DestAdd$y <- c(DestAdd$y, input$AddDest$y))
+      })
+      
+      observe({
+        # Initially will be empty
+        if (is.null(input$DeleteDest)){
+          return() 
+        }
+        isolate(DestDelete$x <- c(DestDelete$x, input$DeleteDest$x))
+        isolate(DestDelete$y <- c(DestDelete$y, input$DeleteDest$y))
+      })
+      
+      
+      DATASTATES2 <- reactive({
+        if(!is.null(input$AddDest)){
+        AddDestStates <- data.frame(state=map.where(states,
+                                                    x=DestAdd$x,
+                                                    y=DestAdd$y))
+        AddDestStates$Value <- 1
+        AddDestStates <- AddDestStates[complete.cases(AddDestStates),]
+        } else{AddDestStates <- data.frame(state="California",Value=0)}
+        if(!is.null(input$DeleteDest)){
+        DeleteDestStates <- data.frame(state=map.where(states,
+                                                       x=DestDelete$x,
+                                                       y=DestDelete$y))
+        DeleteDestStates$Value <- -1
+        DeleteDestStates <- DeleteDestStates[ complete.cases(DeleteDestStates),]
+        } else{DeleteDestStates <- data.frame(state="California",Value=0)}
+        
+        ####figure out the current state of the clicks by mappking (k,v) pairs
+        STATES <- bind_rows(AddDestStates,DeleteDestStates) %>% 
+          group_by(state) %>%
+          summarise(count=sum(Value) )%>% 
+          filter(count>=1)
+        if(nrow(STATES)>0){
+        mapDest <- map("state",regions = STATES$state,plot=F,fill=T,col="grey")} else{mapDest <- NULL}
+        list(mapDest=mapDest)
+      })
+      
+      output$DestPlotAdd <- renderPlot(function(){
+        mapDest <- DATASTATES2()[["mapDest"]]
+        map(states)
+        if(!is.null(mapDest)){
+          map(mapDest,fill=T,add=T,col="grey")
+        }
+        map.text(state_labs,add=T)
+      })
+        
+        
+      output$DestPlotDelete <- renderPlot(function(){
+        mapDest <- DATASTATES2()[["mapDest"]]
+        map(states)
+        if(!is.null(mapDest)){
+          map(mapDest,fill=T,add=T,col="grey")
+        }
+        map.text(state_labs,add=T)
         })
+      
+
   
   
 })###end server here
--- trunk/ShinyDeployModel/ui.R
+++ trunk/ShinyDeployModel/ui.R
@@ -1,31 +1,85 @@
 
+header <- dashboardHeader(
+  title="CPDS Time Series"
+  
+  )###end header
 
-# Define UI for miles per gallon application
-shinyUI(fluidPage(
-  fluidRow(column(3,
-           h2("What Ever stuff goes here")
-           
-           ),###end left hand nav bar
-           column(9, 
-                  h2("Add Map"),
-                  plotOutput(outputId = "OrigPlotAdd", "100%", "500px",  clickId = "AddOrigin"),
-                  h2("Remove Map"),
-                  plotOutput(outputId = "OrigPlotDelete", "100%", "500px", clickId = "DeleteOrigin")
-           )
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Select Geography", tabName = "geography", icon = icon("fa fa-globe")),
+    menuItem("Data Conditioning", tabName = "data_conditioning", icon = icon("fa fa-line-chart"))
   )
-)
-)
+  
+)###end side bar
+
+body <- dashboardBody(
+
+  tabItems(
+    tabItem(tabName = "geography",
+            fluidRow(
+              column(width=6,
+                     box(
+                       title="Add Origin State",width=NULL,status="primary",
+                       solidHeader = TRUE,
+                       plotOutput(outputId = "OrigPlotAdd",clickId = "AddOrigin")
+                        ),
+                     box(
+                       title="Remove Origin State",width=NULL,status="primary",
+                       solidHeader = TRUE,
+                       plotOutput(outputId = "OrigPlotDelete", clickId = "DeleteOrigin")
+                        )
+                     ),###end first column
+              column(width=6,
+                     box(
+                       title="Add Destination State",width=NULL,status="primary",
+                       solidHeader = TRUE,
+                       plotOutput(outputId = "DestPlotAdd",clickId = "AddDest")
+                     ),
+                     box(
+                       title="Remove Destination State",width=NULL,status="primary",
+                       solidHeader = TRUE,
+                       plotOutput(outputId = "DestPlotDelete", clickId = "DeleteDest")
+                     )
+              )###end second column
+            )
+    ),
+    
+    tabItem(tabName = "data_conditioning",
+            h2("Data Selection")
+    )
+  )
+)###end body
+
+
+
+dashboardPage(header, sidebar, body)
+
+
+
+
+
+# shinyUI(fluidPage(
+#   fluidRow(column(3,
+#            h2("What Ever stuff goes here")
+#            
+#            ),###end left hand nav bar
+#            column(4.5, 
+#                   bsCollapse(multiple = T,open="state",id="map_selector",
+#                              bsCollapsePanel("States",
+#                                              h2("Add Map"),
+#                                              plotOutput(outputId = "OrigPlotAdd",clickId = "AddOrigin"),
+#                                              h2("Remove Map"),
+#                                              plotOutput(outputId = "OrigPlotDelete", clickId = "DeleteOrigin"),
+#                                              id="state",value="test1"
+#                                              )
+#                   )
+#            ),
+#            column(4.5, h2("dest goes here")
+#            )
+#   )
+# ))
 
   
   
   
-#   h2("Central Maine Power Live Outage Map"),
-#   htmlOutput('ts'),
-#   plotOutput(outputId = "cmpPlot", height="650px",clickId = "MapClick"),
-#   plotOutput(outputId = "cmpPlot2", height="650px",clickId = "MapClick2"),
-#   dataTableOutput('click_behavior'),
-#   dataTableOutput('click_behavior2'),
-#   dataTableOutput('details'),
-#   HTML("<hr noshade size=1><center>Made by <a href='http://twitter.com/hrbrmstr'>@hrbrmstr</a>; Source at: <a href='https://gist.github.com/hrbrmstr/7681842'>github</a></center>")
-#   
-# ))
+
