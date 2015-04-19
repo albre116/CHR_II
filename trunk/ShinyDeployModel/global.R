9fb6b0620495dbc19f861d88e52822d88bbe74e2
albre116 <albre116@gmail.com>
2015-04-18 21:51:17 -0500

    FULL DATA

--- trunk/ShinyDeployModel/global.R
+++ trunk/ShinyDeployModel/global.R
@@ -92,7 +92,7 @@ if(!require('dyPencilgraphs')) {
 
 
 ####Load Data File
-load("RAW_10.RData")
+load("RAW_100_New2.RData")
 
 
 states.model <- c("Washington", "Montana", "Maine", "North Dakota", "South Dakota",
